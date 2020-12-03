53b73776d95298f1d8e92ab104320cc6ff7512dd
Ignacio Quinteros <i.quinteros@hotmail.com>
2020-12-03 19:19:16 -0300

    Details applied

--- app/src/main/java/com/iquinteros/lodgefinder/MainActivity.java
+++ app/src/main/java/com/iquinteros/lodgefinder/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Desarrollado por Ignacio Quinteros y Joaquín Ruíz", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
